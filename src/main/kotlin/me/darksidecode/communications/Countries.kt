06c787d44b8fe0ebfdb8c81be9d7cdc3863bc817
German Vekhorev <darksidecode@ya.ru>
2023-04-17 16:54:42 +0200

    Example gif

--- src/main/kotlin/me/darksidecode/communications/Countries.kt
+++ src/main/kotlin/me/darksidecode/communications/Countries.kt
@@ -33,9 +33,20 @@ object Japan : CountryInfo {
         lbl1.font = Font("Segoe UI", Font.BOLD, 24)
         contents.add(lbl1, "wrap")
 
-        val img1 = ImageIO.read(javaClass.classLoader.getResource("test.png"))
-            .getScaledInstance(250, 200, Image.SCALE_SMOOTH)
-        contents.add(JLabel(ImageIcon(img1)), "wrap")
+//        val img1 = ImageIO.read(javaClass.getResource("/gigacapy.gif"))
+//            .getScaledInstance(250, 200, Image.SCALE_SMOOTH)
+
+        var imageIcon = ImageIcon(javaClass.getResource("/gigacapy.gif")) // load the image to a imageIcon
+
+        val image = imageIcon.image // transform it
+
+        val newimg = image.getScaledInstance(250, 200, Image.SCALE_DEFAULT) // scale it the smooth way
+
+        imageIcon = ImageIcon(newimg) // transform it back
+
+
+        //val img1 = ImageIcon(javaClass.getResource("/gigacapy.gif"))
+        contents.add(JLabel(imageIcon), "wrap")
 
         val lbl2 = JLabel("<html><p>line 1<p>line <b>two</b>2 <p>sdfsdfdsdfsfs<p>sdfsdfqe<p>qeqwe<p>adasd<p>zfzxzxzc<p>qeqweqwedf</html>")
         lbl2.font = Font("Segoe UI", Font.PLAIN, 18)
